d4a32bcdc70962c1c86f3b07fa8f17f5da61a627
Erin Lincoln <el77717@gmail.com>
2022-04-24 22:11:27 -0400

    start of visualizer

--- rb_theme.js
+++ rb_theme.js
@@ -1,93 +1,229 @@
-function drawLine(ctx, begin, end, stroke = 'black', width = 1) {
-    if (stroke) {
-        ctx.strokeStyle = stroke;
-    }
+const d3 = require('d3')
 
-    if (width) {
-        ctx.lineWidth = width;
-    }
 
-    ctx.beginPath();
-    ctx.moveTo(...begin);
-    ctx.lineTo(...end);
-    ctx.stroke();
+// this clears the svg that Sterling provides to us.
+d3.select(svg).selectAll("*").remove();
+
+// constants
+const RED = "#B22222";
+const BLACK = "#000000";
+const h_margin = 600;
+const w_margin = 100;
+const radius = 20;
+const numInst = instances.length
+
+function instanceToTree (inst) {
+    // get sig insances
+    const atoms = inst.signature("Node").atoms(true);
+    const nodes = atoms.map(atom => ({ id: atom.id()}));
+
+    // get root
+    const roots = inst.field('rootNode').tuples();
+    const root = roots.map(tuple => { 
+        const atoms = tuple.atoms();
+        return {
+            id: atoms[1].id()
+        }
+    })[0];
+
+    // convert tuple to next
+    const ltuples = inst.field('left').tuples();
+    const lefts = ltuples.map(tuple => {
+        const atoms = tuple.atoms();
+        return {
+         source: atoms[0].id(),
+         target: atoms[1].id()
+        }
+    });
+    const left = Object.assign({}, ...lefts.map((x) => ({[x.source]: x.target})));
+
+    const rtuples = inst.field('right').tuples();
+    const rights = rtuples.map(tuple => {
+        const atoms = tuple.atoms();
+        return {
+         source: atoms[0].id(),
+         target: atoms[1].id()
+        }
+    });
+    const right = Object.assign({}, ...rights.map((x) => ({[x.source]: x.target})));
+
+
+    // get colors of nodes
+    const ctuples = inst.field('color').tuples();
+    const color = ctuples.map(tuple => {
+        const atoms = tuple.atoms();
+        return {
+         n: atoms[0].id(),
+         c: atoms[1].id()
+        }
+    });
+    const colors = Object.assign({}, ...color.map((x) => ({[x.n]: x.c})));
+
+    const vtuples = inst.field('value').tuples();
+    const value = vtuples.map(tuple => {
+        const atoms = tuple.atoms();
+        return {
+         n: atoms[0].id(),
+         v: atoms[1].id()
+        }
+    });
+    const values = Object.assign({}, ...value.map((x) => ({[x.n]: x.v})));
+    
+    return [nodes, root, left, right, colors, values];
 }
 
-class RBTree{
+function setNodeXY (root, left, right, inst) {
+    const x = new Map();
+    const y = new Map();
+    const n = [];
+    const insth = getKey(instances, inst);
 
-    constructor(){
-        this.startPosition = {x: 800, y: 44}
-        this.axisX = 350
-        this.axisY = 80
-        this.positions = {}
+
+    function treeHeight(node) {
+        var l = 0;
+        var r = 0;
+        if (left[node] != null) {
+            l = treeHeight(left[node]);
+        }
+        if (right[node] != null) {
+            r = treeHeight(right[node]);
+        }
+
+        if (r > l) {
+            return r + 1;
+        } else {
+            return l + 1;
+        }
     }
 
-    getPosition({x , y}, isLeft = false){
-        return { x: isLeft ? x - this.axisX + y : x + this.axisX - y, y: y + this.axisY }
+    const height = treeHeight(root.id) + 1;
+
+    function xleft(node) {
+        return x.get(node) - (treeHeight(node)/height)*w_margin; 
+    }
+    function xright(node) {
+        return x.get(node) + (treeHeight(node)/height)*w_margin; 
     }
 
-    add(n){
-        if (left.tuples()[n]){
-            this.positions[left.tuples()[n]] = this.getPosition(this.positions[n])
-            this.add(left.tuples()[left.tuples()[n]])
+    function yval(node) {
+        return (y.get(node) + h_margin/height/numInst); 
+    }
+
+    function xyHelper(node) {
+        if (left[node] != null) {
+            x.set(left[node], xleft(node));
+            // x[left[node]] = x[node] - w_margin;
+            y.set(left[node], yval(node));
+            // y[left[node]] = y[node] + h_margin;
+            // alert(left[node])
+            n.push(left[node]);
+            xyHelper(left[node]);
         }
-        if (right.tuples()[n]) {
-            this.positions[right.tuples()[n]] = this.getPosition(this.positions[n])
-            this.add(right.tuples()[right.tuples()[n]])
+        if (right[node] != null) {
+            x.set(right[node], xright(node));
+            // x[right[node]] = x[node] + w_margin;
+            y.set(right[node], yval(node));
+            // y[right[node]] = y[node] + h_margin;
+            n.push(right[node]);
+            xyHelper(right[node]);
         }
+    }
+
+    n.push(root.id)
+    x.set(root.id, 250);
+    y.set(root.id, 40 + (h_margin/numInst)*insth);
+    xyHelper(root.id)
+    console.log(y)
+    return [x,y,n]
+}
+
+function getKey(obj, val) {
+    for (var key in Object.keys(obj)) {
+            if (obj[Object.keys(obj)[key]] === val)
+                return Object.keys(obj)[key];
+            }
+}
+
+function parent(node, left, right) {
+    var ln = getKey(left, node)
+    var rn = getKey(right, node)
+
+
+    if (ln != null) {
+        return ln
+    } else if (rn != null) {
+        return rn
+    } else {
+        return null
+    }
+}
+
+function graph(inst) {
+    var [inodes, iroot, ileft, iright, icolors, ivalues] = instanceToTree(inst);
+    var [ix, iy, inode] = setNodeXY(iroot, ileft, iright, inst);
+
+    d3.select(svg)
+        .selectAll("lines")
+        .data(inode)
+        .join("line")
+        .style("stroke-width", 10)
+        .style("stroke", BLACK)
+        .attr("x1", (d, i) => {
+            return ix.get(d) - 5
+        })
         
-    }
-
-    setPositions(){
-        this.positions[Root] = this.startPosition
-        this.add(Root)
-    }
-
-    drawNode(ctx, n) {
-        var X = this.positions[n][0];
-        var Y = this.positions[n][1];
-        var R = 5;
-        ctx.beginPath();
-        ctx.arc(X, Y, R, 0, 2 * Math.PI);
-        ctx.lineWidth = 3;
-        if (color.tuples()[n] == "Red") {
-            ctx.fillStyle = '#990000';
-        } 
-        else {
-            ctx.fillStyle = '#000000';
-        }
-        ctx.fill();
-
-        ctx.fillStyle = "#FFFFFF";
-        ctx.fillText(value.tuples()[n], X, Y);
-
-        if (left.tuples()[n]) {
-            ctx.beginPath();
-            ctx.moveTo(this.positions[n]);
-            ctx.lineTo(this.positions[left.tuples()[n]])
-            ctx.stroke();
-        }
-
-        if (right.tuples()[n]) {
-            ctx.beginPath();
-            ctx.moveTo(this.positions[n]);
-            ctx.lineTo(this.positions[right.tuples()[n]])
-            ctx.stroke();
-        }
-    }
-
-    generate(){
-        this.setPositions()
-        var canvas = document.getElementById('canvas');
-        print(canvas)
-        var ctx = canvas.getContext('2d'); 
-
-        for (n in this.positions.keys()) {
-            this.drawNode(ctx, n)
-        }
-    }
+        .attr("y1", (d, i) => {
+            return iy.get(d) + 5
+        })
+        .attr("x2", (d, i) => {
+            if (parent(d, ileft, iright) != null) {
+                return ix.get(parent(d, ileft, iright)) - 5
+            } else {
+                return ix.get(d) - 5
+            }
+            
+        })
+        .attr("y2", (d, i) => {
+            if (parent(d, ileft, iright) != null) {
+                return iy.get(parent(d, ileft, iright)) - 5
+            } else {
+                return iy.get(d) - 5
+            }
+            
+        });
+    d3.select(svg)
+        .selectAll("nodes")
+        .data(inode)
+        .join("circle")
+        .attr("cx", (d, i) => {
+            return ix.get(d)
+        })
+        .attr("cy", (d, i) => {
+            return iy.get(d)
+        })
+        .attr("r", radius)
+        .attr("fill", (d, i) => {
+            if (icolors[d] == "Red0") {
+                return RED;
+            }
+            if (icolors[d] == "Black0") {
+                return BLACK;
+            }
+        });
+    d3.select(svg)
+        .selectAll("values")
+        .data(inode)
+        .join("text")
+        .attr("x", (d, i) => {
+            return ix.get(d) - 5;
+        })
+        .attr("y", (d, i) => {
+            return iy.get(d) + 5;
+        })
+        .text((d, i) => {
+            return ivalues[d];
+        })
+        .attr("fill", "#ffffff");
 }
 
-
-const t = new RBTree()
-t.generate()
+instances.map(graph)
